175aa3e59257cdb01e13d406a056ec145ecaf140
TrashNokdu <72365216+Trashnokdu@users.noreply.github.com>
2023-03-29 08:53:53 +0900

    delete Chartdata
    3집을 기원하며...

--- js/detector.jsx
+++ js/detector.jsx
@@ -31,9 +31,6 @@ function render(){
 
 const Topbar = () => <div style={{display: "flex", height: "15vh"}}>
 <h1 style={{marginLeft: "3%", fontSize: "1.9vh"}}>욕시그 검사기</h1>
-<h1 id="rankru" style={{marginLeft: "10%", fontSize: "2vh", height: "2vh"}}>로딩중...</h1>
-<h1 id="rankre" style={{marginLeft: "10%", fontSize: "2vh", height: "2vh"}}>로딩중...</h1>
-<h1 id="rankws" style={{marginLeft: "10%", fontSize: "2vh", height: "2vh"}}>로딩중...</h1>
 </div>
 const TextInput = () => <div>
     <textarea id="str" style={{height: "60vh", width: "100%", fontFamily: "Nanum Gothic", fontSize: "24pt"}} placeholder="여기 안에 검사할 택스트를 입력해주세요"></textarea>
@@ -57,9 +54,9 @@ render()
 // ----------------------------------------------------------------------
 let result;
 const textarea = document.getElementById("str");
-const rankru = document.getElementById("rankru");
-const rankre = document.getElementById("rankre");
-const rankws = document.getElementById("rankws");
+// const rankru = document.getElementById("rankru");
+// const rankre = document.getElementById("rankre");
+// const rankws = document.getElementById("rankws");
 const apilink = "https://podoalforbidden-gtsni.run.goorm.site"
 const textareaValue = textarea.value;
 function getfor(str) {
@@ -68,23 +65,23 @@ function getfor(str) {
         .then(data => textarea.value = data)
         .catch(error => console.error(error));
 }
-function getchart(songn, h1){
-    return fetch(apilink + "/api/get/chart?songn=" + encodeURIComponent(songn))
-        .then(response => response.json())
-        .then(data => {h1.innerText=data;})
-        .catch(error => console.error(error));
-}
+// function getchart(songn, h1){
+//     return fetch(apilink + "/api/get/chart?songn=" + encodeURIComponent(songn))
+//         .then(response => response.json())
+//         .then(data => {h1.innerText=data;})
+//        .catch(error => console.error(error));
+// }
 function reset() {
     textarea.value = "";
 }
 
-function reqtest() {
-    var getdata = getfor(textarea.value);
-}
-function copy(){
-    textarea.select();
-    document.execCommand('copy');
-}
-getchart("잠깐 나올래", rankru);
-getchart("리와인드", rankre);
-getchart("겨울봄", rankws);
+// function reqtest() {
+//     var getdata = getfor(textarea.value);
+// }
+// function copy(){
+//     textarea.select();
+//     document.execCommand('copy');
+// }
+// getchart("잠깐 나올래", rankru);
+// getchart("리와인드", rankre);
+// getchart("겨울봄", rankws);
