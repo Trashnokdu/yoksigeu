68b8a72ad106365b32bcdc156dbd9f046f76b9d8
녹두레기 <72365216+Trashnokdu@users.noreply.github.com>
2023-12-04 18:11:55 +0900

    feat: mollu

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     toast("완료되었습니다! 하단의 복사버튼으로 복사하세요!")
   }
   const getfor = () => {
-    return fetch("https://api.yoksigeu.xyz:3000" + "/api/get/detect?str=" + text)
+    return fetch("https://api.yoksigeu.xyz:8443" + "/api/get/detect?str=" + text)
     .then(response => response.json())
     .then(data => {
-      text = data
+      setText(data);
       donenotify()
     })
     .catch(error => console.error(error));
@@ -43,14 +43,27 @@ function App() {
         </nav>
         <textarea 
           id="str" 
-          style={{height: "84vh", width: "100%", fontSize: "1rem", resize: "none", border: "none", outline: "none"}} 
+          style={{height: "74vh", width: "100%", fontSize: "1rem", resize: "none", border: "none", outline: "none"}} 
           placeholder="여기 안에 검사할 텍스트를 입력해주세요"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
         <div>
-        <button className='btn' id='detect' onClick={getfor} style={{height: "9.2vh", width: "50%", backgroundColor: '#0F0F0F', borderRadius: "0", color: "white", fontSize: "2em", borderRight: "solid gray"}}>검사하기</button>
-        <button className='btn' id='copy' onClick={copy} style={{height: "9.2vh", width: "50%", backgroundColor: '#0F0F0F', borderRadius: "0", color: "white", fontSize: "2em"}}>복사하기</button>
+        <button className='btn' id='detect' onClick={getfor} style={{height: "9.2%", width: "50%", backgroundColor: '#0F0F0F', borderRadius: "0", color: "white", fontSize: "2em", borderRight: "solid gray"}}>검사하기</button>
+        <button className='btn' id='copy' onClick={copy} style={{height: "9.2%", width: "50%", backgroundColor: '#0F0F0F', borderRadius: "0", color: "white", fontSize: "2em"}}>복사하기</button>
+        </div>
+        <div>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8338001638680274"
+             crossOrigin="anonymous"></script>
+        <ins className="adsbygoogle"
+             style={{display: 'block'}}
+             data-ad-format="fluid"
+             data-ad-layout-key="-fb+5w+4e-db+86"
+             data-ad-client="ca-pub-8338001638680274"
+             data-ad-slot="9898474422"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         </div>
     </div>
   );
